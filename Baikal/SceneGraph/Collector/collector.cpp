554d8b3a9c69835d92c824e9577f43b5d96da2a5
Denis Biryukov <dbirukov@luxoft.com>
2018-03-12 11:47:15 +0300

    Switch to unordered_map in collector

--- Baikal/SceneGraph/Collector/collector.cpp
+++ Baikal/SceneGraph/Collector/collector.cpp
@@ -3,19 +3,20 @@
 #include "SceneGraph/iterator.h"
 #include <vector>
 #include <cassert>
+#include <unordered_map>
 
 namespace Baikal
 {
-    struct IdCompare 
+    struct IdCompare
     {
         bool operator() (const SceneObject::Ptr& lhs, const SceneObject::Ptr& rhs) const
         {
             return lhs->GetId() < rhs->GetId();
         }
     };
-    using ItemMap = std::map<SceneObject::Ptr, int>;
+    using ItemMap = std::unordered_map<uint32_t, int>;
     using ItemSet = std::set<SceneObject::Ptr, IdCompare>;
-
+    
     class BundleImpl : public Bundle
     {
     public:
@@ -24,30 +25,30 @@ namespace Baikal
         , m_set(set)
         {
         }
-
+        
         ItemMap m_map;
         ItemSet m_set;
     };
-
+    
     struct Collector::CollectorImpl
     {
         ItemMap m_map;
         ItemSet m_set;
     };
-
+    
     Collector::Collector()
     : m_impl (new CollectorImpl)
     {
     }
-
+    
     Collector::~Collector() = default;
-
+    
     void Collector::Clear()
     {
         m_impl->m_map.clear();
         m_impl->m_set.clear();
     }
-
+    
     std::unique_ptr<Iterator> Collector::CreateIterator() const
     {
         return std::unique_ptr<Iterator>(
@@ -61,7 +62,7 @@ namespace Baikal
         {
             // Expand current item
             auto cur_items = expand_func(iter.Item());
-
+            
             // Insert items
             m_impl->m_set.insert(cur_items.cbegin(), cur_items.cend());
         }
@@ -75,14 +76,14 @@ namespace Baikal
     void Collector::Commit()
     {
         m_impl->m_map.clear();
-
+        
         int idx = 0;
         for (auto& i : m_impl->m_set)
         {
-            m_impl->m_map[i] = idx++;
+            m_impl->m_map[i->GetId()] = idx++;
         }
     }
-
+    
     void Collector::Finalize(FinalizeFunc finalize_func)
     {
         for (auto& i : m_impl->m_set)
@@ -90,11 +91,11 @@ namespace Baikal
             finalize_func(i);
         }
     }
-
+    
     bool Collector::NeedsUpdate(Bundle const* bundle, ChangedFunc changed_func) const
     {
         auto bundle_impl = static_cast<BundleImpl const*>(bundle);
-
+        
         // Check if:
         // 0) bundle and our map sizes match.
         // 1) All the objects collector has are in the bundle.
@@ -103,7 +104,7 @@ namespace Baikal
         {
             return true;
         }
-
+        
         for (auto& i : m_impl->m_set)
         {
             if (changed_func(i))
@@ -111,9 +112,9 @@ namespace Baikal
                 // Case 2: we have the object which is changed.
                 return true;
             }
-
+            
             auto iter = bundle_impl->m_set.find(i);
-
+            
             if (iter == bundle_impl->m_set.cend())
             {
                 // Case 1: we have an object which is not serialized as a part of bundle.
@@ -123,31 +124,31 @@ namespace Baikal
             // Here we know that bundle_impl->m_map[i] == m_impl->m_map[i]
             // since it is defined by set traversal order.
         }
-
+        
         return false;
     }
-
+    
     std::size_t Collector::GetNumItems() const
     {
         return m_impl->m_set.size();
     }
-
+    
     Bundle* Collector::CreateBundle() const
     {
         return new BundleImpl { m_impl->m_map, m_impl->m_set };
     }
-
+    
     std::uint32_t Collector::GetItemIndex(SceneObject::Ptr item) const
     {
-        auto iter = m_impl->m_map.find(item);
-
+        auto iter = m_impl->m_map.find(item->GetId());
+        
         if (iter == m_impl->m_map.cend())
         {
             throw std::runtime_error("No such item in the collector");
         }
-
+        
         return iter->second;
     }
-
-
+    
+    
 }
