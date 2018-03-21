a359b2b9c564694716fec8da8b1ac24da3642e1a
Denis Biryukov <dbirukov@luxoft.com>
2018-03-21 15:45:55 +0300

    Fix OIIO module

--- cmake/FindOIIO.cmake
+++ cmake/FindOIIO.cmake
@@ -4,7 +4,5 @@ find_library(OIIO_LIB OpenImageIO HINTS ${OIIO_DIR}/lib/x64)
 
 add_library(OpenImageIO SHARED IMPORTED)
 set_target_properties(OpenImageIO PROPERTIES LOCATION ${OIIO_LIB})
-set_target_properties(OpenImageIO PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${OIIO_DIR}/include
-    INTERFACE_LINK_LIBRARIES OpenImageIO::OpenImageIO)
-    
+set_target_properties(OpenImageIO PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${OIIO_DIR}/include)
+   
