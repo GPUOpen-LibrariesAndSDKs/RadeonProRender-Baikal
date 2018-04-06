ff8ef4f346fb2d06fca5487387462df6c020ca03
Denis Biryukov <dbirukov@luxoft.com>
2018-04-06 15:23:28 +0300

    Add GetBxDFType and PrepareInputs generation functions

--- Baikal/Utils/cl_uberv2_generator.cpp
+++ Baikal/Utils/cl_uberv2_generator.cpp
@@ -53,7 +53,107 @@ void CLUberV2Generator::AddMaterial(UberV2Material::Ptr material)
 
 void CLUberV2Generator::MaterialGeneratePrepareInputs(UberV2Material::Ptr material, UberV2Sources *sources)
 {
+    static const std::string reader_function_arguments("material_attributes[offset++], dg, input_map_values, TEXTURE_ARGS");
+    struct LayerReader
+    {
+        std::string variable;
+        std::string reader;
+    };
+    // Should have same order as in ClwSceneController::WriteMaterial
+    static const std::vector<std::pair<UberV2Material::Layers, std::vector<LayerReader>>> readers =
+    {
+        {
+            UberV2Material::Layers::kEmissionLayer,
+            {
+                {"shader_data.emission_color", "GetInputMapFloat4"}
+            }
+        },
+        {
+            UberV2Material::Layers::kCoatingLayer,
+            {
+                {"shader_data.coating_color", "GetInputMapFloat4"},
+                {"shader_data.coating_ior", "GetInputMapFloat"}
+            }
+        },
+        {
+            UberV2Material::Layers::kReflectionLayer,
+            {
+                {"shader_data.coating_color", "GetInputMapFloat4"},
+                {"shader_data.coating_ior", "GetInputMapFloat"},
+                {"shader_data.reflection_color", "GetInputMapFloat4"},
+                {"shader_data.reflection_roughness", "GetInputMapFloat"},
+                {"shader_data.reflection_anisotropy", "GetInputMapFloat"},
+                {"shader_data.reflection_anisotropy_rotation", "GetInputMapFloat"},
+                {"shader_data.reflection_ior", "GetInputMapFloat"},
+                {"shader_data.reflection_metalness", "GetInputMapFloat"}
+            }
+        },
+        {
+            UberV2Material::Layers::kDiffuseLayer,
+            {
+                {"shader_data.diffuse_color", "GetInputMapFloat4"}
+            }
+        },
+        {
+            UberV2Material::Layers::kRefractionLayer,
+            {
+                {"shader_data.refraction_color", "GetInputMapFloat4"},
+                {"shader_data.refraction_roughness", "GetInputMapFloat"},
+                {"shader_data.refraction_ior", "GetInputMapFloat"}
+            }
+        },
+        {
+            UberV2Material::Layers::kTransparencyLayer,
+            {
+                {"shader_data.transparency", "GetInputMapFloat"}
+            }
+        },
+        {
+            UberV2Material::Layers::kShadingNormalLayer,
+            {
+                {"shader_data.shading_normal", "GetInputMapFloat4"},
+            }
+        },
+        {
+            UberV2Material::Layers::kSSSLayer,
+            {
+                {"shader_data.sss_absorption_color", "GetInputMapFloat4"},
+                {"shader_data.sss_scatter_color", "GetInputMapFloat4"},
+                {"shader_data.sss_subsurface_color", "GetInputMapFloat4"},
+                {"shader_data.sss_absorption_distance", "GetInputMapFloat"},
+                {"shader_data.sss_scatter_distance", "GetInputMapFloat"},
+                {"shader_data.sss_scatter_direction", "GetInputMapFloat"},
+            }
+        }
+    };
 
+
+    // Reserve 4k. This should be enought for maximum configuration
+    sources->m_prepare_inputs.reserve(4096);
+    // Write base code
+    uint32_t layers = material->GetLayers();
+    sources->m_prepare_inputs =
+        "UberV2ShaderData UberV2PrepareInputs" + std::to_string(layers) + "("
+        "DifferentialGeometry const* dg, GLOBAL InputMapData const* restrict input_map_values,"
+        "GLOBAL int const* restrict material_attributes, TEXTURE_ARG_LIST)\n"
+        "{\n"
+        "\tUberV2ShaderData shader_data;\n"
+        "\tint offset = dg->mat.offset + 1;\n";
+
+    // Write code for each layer
+    for (auto &reader: readers)
+    {
+        // If current layer exist in material - write it
+        if ((layers & reader.first) == reader.first)
+        {
+            for (auto &r : reader.second)
+            {
+                sources->m_prepare_inputs += std::string("\t") + r.variable + " = " + r.reader + "(" + reader_function_arguments + ");\n";
+            }
+        }
+    }
+
+    sources->m_prepare_inputs += "\treturn shader_data;\n}";
 }
 
 void CLUberV2Generator::MaterialGenerateGetPdf(UberV2Material::Ptr material, UberV2Sources *sources)
@@ -63,12 +163,122 @@ void CLUberV2Generator::MaterialGenerateGetPdf(UberV2Material::Ptr material, Ube
 
 void CLUberV2Generator::MaterialGenerateSample(UberV2Material::Ptr material, UberV2Sources *sources)
 {
+    uint32_t layers = material->GetLayers();
+    sources->m_sample =
+        "float3 UberV2_Sample(DifferentialGeometry const* dg, float3 wi, TEXTURE_ARG_LIST, float2 sample, float3* wo, float* pdf,
+        "UberV2ShaderData const* shader_data)\n"
+        "{\n"
+        "\tconst int sampledComponent = Bxdf_UberV2_GetSampledComponent(dg);\n";
+
+
+
+
+
+
+
+
+    soruces->m_sample +=
+        "\treturn 0.f;\n"
+        "}\n";
 
 }
-
 void CLUberV2Generator::MaterialGenerateGetBxDFType(UberV2Material::Ptr material, UberV2Sources *sources)
 {
 
+    uint32_t layers = material->GetLayers();
+    sources->m_get_bxdf_type = "void GetMaterialBxDFType" + std::to_string(layers) + "("
+        "float3 wi, Sampler* sampler, SAMPLER_ARG_LIST, DifferentialGeometry* dg, UberV2ShaderData const* shader_data)\n"
+        "{\n";
+        "\tint bxdf_flags = 0;\n";
+
+    // If we have layer that requires fresnel blend (reflection/refraction/coating)
+    if ((layers &
+        (UberV2Material::Layers::kReflectionLayer | UberV2Material::Layers::kRefractionLayer | UberV2Material::Layers::kCoatingLayer ))
+        != 0)
+    {
+
+        sources->m_get_bxdf_type += "\tconst float ndotwi = dot(dg->n, wi);\n";
+    }
+
+    if ((layers & UberV2Material::Layers::kEmissionLayer) == UberV2Material::Layers::kEmissionLayer)
+    {
+        sources->m_get_bxdf_type += "\tbxdf_flags |= kBxdfFlagsEmissive;\n";
+    }
+
+    if ((layers & UberV2Material::Layers::kTransparencyLayer) == UberV2Material::Layers::kTransparencyLayer)
+    {
+        // If layer have transparency check in OpenCL if we sample it and if we sample it - return
+        sources->m_get_bxdf_type +=
+            "\tfloat sample = Sampler_Sample1D(sampler, SAMPLER_ARGS);\n"
+            "\tif (sample < shader_data->transparency)\n"
+            "\t{\n"
+            "\t\tbxdf_flags |= (kBxdfFlagsTransparency | kBxdfFlagsSingular);\n"
+            "\t\tBxdf_SetFlags(dg, bxdf_flags);\n"
+            "\t\tBxdf_UberV2_SetSampledComponent(dg, kBxdfUberV2SampleTransparency);\n"
+            "\t\treturn;\n"
+            "\t}\n";
+    }
+
+    // Check refraction layer. If we have it and plan to sample it - set flags and sampled component
+    if ((layers & UberV2Material::Layers::kRefractionLayer) == UberV2Material::Layers::kRefractionLayer)
+    {
+        sources->m_get_bxdf_type +=
+            "\tfloat sample = Sampler_Sample1D(sampler, SAMPLER_ARGS);\n"
+            "\tconst float fresnel = CalculateFresnel(1.0f, shader_data->refraction_ior, ndotwi);\n"
+            "\tif (sample >= fresnel)\n"
+            "\t{\n"
+            "\t\tBxdf_UberV2_SetSampledComponent(dg, kBxdfUberV2SampleRefraction);\n"
+            "\t\tif (shader_data->refraction_roughness < ROUGHNESS_EPS)\n"
+            "\t\t{\n"
+            "\t\t\tbxdf_flags |= kBxdfFlagsSingular;\n"
+            "\t\t}\n"
+            "\t\tBxdf_SetFlags(dg, bxdf_flags);"
+            "\t\treturn;\n"
+            "\t}\n";
+    }
+
+    sources->m_get_bxdf_type +=
+        "\tbxdf_flags |= kBxdfFlagsBrdf;\n"
+        "\tfloat top_ior = 1.0f;\n";
+
+    if ((layers & UberV2Material::Layers::kCoatingLayer) == UberV2Material::Layers::kCoatingLayer)
+    {
+        sources->m_get_bxdf_type +=
+            "\tfloat sample = Sampler_Sample1D(sampler, SAMPLER_ARGS);\n"
+            "\tconst float fresnel = CalculateFresnel(top_ior, shader_data->coating_ior, ndotwi);\n"
+            "\tif (sample < fresnel)\n"
+            "\t{\n"
+            "\t\tbxdf_flags |= kBxdfFlagsSingular;\n"
+            "\t\tBxdf_SetFlags(dg, bxdf_flags);\n"
+            "\t\tBxdf_UberV2_SetSampledComponent(dg, kBxdfUberV2SampleCoating);\n"
+            "\t\treturn;\n"
+            "\t}\n"
+            "\ttop_ior = shader_data->coating_ior;\n";
+    }
+
+    if ((layers & UberV2Material::Layers::kReflectionLayer) == UberV2Material::Layers::kReflectionLayer)
+    {
+        sources->m_get_bxdf_type +=
+            "\tconst float fresnel = CalculateFresnel(top_ior, shader_data->reflection_ior, ndotwi);\n"
+            "\tfloat sample = Sampler_Sample1D(sampler, SAMPLER_ARGS);\n"
+            "\tif (sample < fresnel)\n"
+            "\t{\n"
+            "\t\tif (shader_data->reflection_roughness < ROUGHNESS_EPS)\n"
+            "\t\t{\n"
+            "\t\t\tbxdf_flags |= kBxdfFlagsSingular;\n"
+            "\t\t}\n"
+            "\t\tBxdf_UberV2_SetSampledComponent(dg, kBxdfUberV2SampleReflection);\n"
+            "\t\tBxdf_SetFlags(dg, bxdf_flags);\n"
+            "\t\treturn;\n"
+            "\t}\n";
+
+    }
+
+    sources->m_get_bxdf_type +=
+        "\tBxdf_UberV2_SetSampledComponent(dg, kBxdfUberV2SampleDiffuse);\n"
+        "\tBxdf_SetFlags(dg, bxdf_flags);\n"
+        "return;\n"
+        "}\n";
 }
 
 void CLUberV2Generator::MaterialGenerateEvaluate(UberV2Material::Ptr material, UberV2Sources *sources)
