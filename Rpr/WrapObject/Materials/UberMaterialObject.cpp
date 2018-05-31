e86574fbfac1e684265cf788804f1d086a847f55
Alexander Veselov <alexander_veselov@yahoo.com>
2018-05-31 11:03:28 +0300

    Remove dynamic cast, exception handling

--- Rpr/WrapObject/Materials/UberMaterialObject.cpp
+++ Rpr/WrapObject/Materials/UberMaterialObject.cpp
@@ -19,19 +19,14 @@ void UberMaterialObject::SetInputF(const std::string & input_name, const RadeonR
 {
     // Optimization: if we create a new input, it will take additional time for kernel compilation,
     // so we try to find existing input and set a new float value
-    try
+    auto& input = m_mat->GetInput(input_name, Baikal::Material::InputType::kInputMap);
+    if (input.value.input_map_value->m_type == Baikal::InputMap::InputMapType::kConstantFloat3)
     {
-        auto& input = m_mat->GetInput(input_name, Baikal::Material::InputType::kInputMap);
-        auto float_map = std::dynamic_pointer_cast<Baikal::InputMap_ConstantFloat3>(input.value.input_map_value);
-        if (float_map == nullptr)
-        {
-            throw std::exception();
-        }
+        auto float_map = std::static_pointer_cast<Baikal::InputMap_ConstantFloat3>(input.value.input_map_value);
         float_map->SetValue(val);
     }
-    catch (std::exception& )
+    else
     {
-        // No float input map found, create a new constant float input
         auto inputMap = Baikal::InputMap_ConstantFloat3::Create(val);
         m_mat->SetInputValue(input_name, inputMap);
     }
